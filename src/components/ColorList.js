724a77a3cfc98146c6ae6d292abf252bfaa8e920
LincKim <lincolnkimutai@gmail.com>
2023-01-13 08:56:56 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+// function ColorItem(props){
+//   return <li style={{ color: props.color}}>{props.color}</li>
+// }
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,6 +13,9 @@ function ColorList() {
     "hotpink",
   ];
 
+    // const colorElements = colors.map((color)=>{
+    //   return <ColorItem key={color} color={color} />
+    // })
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
